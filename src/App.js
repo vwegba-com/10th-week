38a3cc0df0f746242325ba09de074b68ba560d06
vwegba-com <erikejerry@gmail.com>
2021-08-07 11:32:21 +0100

    week10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Login from './components/Login';
+import { Switch,Route,Link} from "react-router-dom";
 
 function App() {
-  return (
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navig">
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <h1>FRONTEND DEVELOPMENT</h1>
+      <Switch>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/contact">
+        <Contact/>
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/">
+        <Home/>
+      </Route>
+
+      </Switch>
+        
     </div>
   );
 }
